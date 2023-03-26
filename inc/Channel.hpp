50bf3eb78b7b884df6b27a4dac7d6bf1ce414f50
Wisterd <lawenna.athonil@gmail.com>
2023-03-26 18:18:56 +0200

    Channel class variables cleaned

--- inc/Channel.hpp
+++ inc/Channel.hpp
@@ -2,30 +2,33 @@
 # define CHANNEL_HPP
 # pragma once
 
+#include "Client.hpp"
 class	Channel {
 	public :
 		Channel();
-		Channel(std::string name, std::string const &topic, std::string const &creator);
+		Channel(std::string name, std::string const &topic, std::string const &creator); 
+		//creator needed to set the first operator
+	
 	private :
 		std::string	_name;
-		std::string	_topic;
-		std::string	_password;
-		std::string	_creator;
-		int			_userCount;
-		int			_userLimit;
+		std::string	_topic; //max 200 characters
+		std::string	_password; //only used if keyMask is set
+		int			_userCount; //number of users in the channel
+		int			_userLimit; //only used if userLimitMask is set
 		//Masks
-		std::string	_banMask; //when set, only users outside of the ban list can join
-		std::string	_inviteMask; //when set, only invited users can join
-		std::string	_voiceMask; //when set, only voiced users can speak
-		std::string _keyMask; //when set, only users with the correct key can join
-		std::string	_privateMask; //when set, channel name won't be displayed in the channel list
-		std::string	_secretMask; //when set, channel name won't be displayed in the channel list nor in the WHO list
+		bool		_banMask; //when set, only users outside of the ban list can join
+		bool		_inviteMask; //when set, only invited users can join
+		bool		_voiceMask; //when set, only voiced users can speak
+		bool 		_keyMask; //when set, only users with the correct key can join
+		bool		_privateMask; //when set, channel topic won't be displayed in the channel list 
+		bool		_secretMask; //when set, channel name won't be displayed in the channel list nor in the WHO list
+		bool		_userLimitMask; //when set, only users up to the user limit can join
 		//Lists
-		std::string	_userList; //list of users in the channel
-		std::string	_banList; //list of users banned from the channel
-		std::string	_inviteList; //list of users invited to the channel
-		std::string	_operatorList; //list of users with operator status
-		std::string	_voiceList; //list of users with voice status
+		std::vector<& Client>	_userList; //list of users in the channel
+		std::vector<& Client>	_banList; //list of users banned from the channel
+		std::vector<& Client>	_inviteList; //list of users invited to the channel
+		std::vector<& Client>	_operatorList; //list of users with operator status
+		std::vector<& Client>	_voiceList; //list of users with voice status
 }
 
 #endif
