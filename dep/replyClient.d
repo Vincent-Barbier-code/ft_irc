bb16f059926e670bbf91e4af163c3dccdead4336
Vincent-Barbier-code <vincentpcrog@hotmail.com>
2023-04-03 14:56:21 +0200

    sans les deps

--- dep/replyClient.d
+++
@@ -1,3 +0,0 @@
-obj/replyClient.o: src/replyClient.cpp inc/server.hpp inc/macro.hpp \
-  inc/ft_irc.hpp inc/utils.hpp inc/client.hpp inc/Channel.hpp \
-  inc/Message.hpp inc/color.hpp
--- dep/server2.d
+++
@@ -1,3 +0,0 @@
-obj/server2.o: src/server2.cpp inc/server.hpp inc/macro.hpp \
-  inc/ft_irc.hpp inc/utils.hpp inc/client.hpp inc/Channel.hpp \
-  inc/Message.hpp inc/color.hpp
