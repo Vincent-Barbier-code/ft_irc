5ac1bd9edae48e08a18af72096845b18d22a198e
Kevin ISIKAYADIR <kisikaya@e1r7p2.clusters.42paris.fr>
2023-04-13 23:45:05 +0200

    Patch nick pour la 1000000 fois sur les nick collision le suffixe de chiffre

--- src/_nick.cpp
+++ src/_nick.cpp
@@ -1,14 +1,26 @@
 #include "Server.hpp"
 
+
 void	Server::_nick(int client_fd, std::string const nick)
 {
 	Client & client = *_clients.at(client_fd);
+
 	if (_findClientByNickName(nick))
 	{
+		size_t end;
+		std::string new_nick = nick;
+
 		int _nickNumber = 1;
-		while (_findClientByNickName(nick + itostr(_nickNumber)))
+		while (_findClientByNickName(new_nick)) {
+			end = new_nick.find_last_not_of("0123456789");
+			new_nick = new_nick.substr(0, end + 1);
+			new_nick += itostr(_nickNumber);
 			_nickNumber++;
-		client.setNickName(nick + itostr(_nickNumber));
+		}
+
+		for(std::map<int, Client *>::iterator receiver = _clients.begin(); receiver != _clients.end(); receiver++)
+		 		client.sendMsgToClient(*receiver->second, "NICK " + new_nick);
+		client.setNickName(new_nick);
 		clerr(ERR_NICKCOLLISION);
 	}
 	else if (nick.size() > MAX_NICKNAME_LENGTH)
